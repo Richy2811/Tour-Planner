bbc6c0d20108b17d67226f0732bf9cdceee0a3c4
DanielSchild25 <daniel.schild25@hotmail.com>
2022-03-09 18:40:05 +0100

    gitignore + Data Binding Data Grid

---
+++ TourPlanner/TourPlanner/LogEntry.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner
+{
+    public class LogEntry : INotifyPropertyChanged
+    {
+        private string _date;
+        private string _duration;
+        private string _distance;
+
+        public LogEntry(string date, string duration, string distance)
+        {
+            this.Date = date;
+            this.Duration = duration;
+            this.Distance = distance;
+        }
+
+        public string Date
+        {
+            get => this._date;
+            set
+            {
+                this._date = value;
+                this.OnPropertyChanged(nameof(Date));
+            }
+        }
+
+        public string Duration
+        {
+            get => this._duration;
+            set
+            {
+                this._duration = value;
+                this.OnPropertyChanged(nameof(Duration));
+            }
+        }
+
+        public string Distance
+        {
+            get => this._distance;
+            set
+            {
+                this._distance = value;
+                this.OnPropertyChanged(nameof(Distance));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ TourPlanner/TourPlanner/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+
+namespace TourPlanner
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<LogEntry> Data { get; }
+            = new ObservableCollection<LogEntry>();
+
+        public string CurrentTitle { get; set; }
+        public RelayCommand AddCommand { get; }
+        public RelayCommand DeleteCommand { get; }
+
+
+        public MainViewModel()
+        {
+            AddCommand = new RelayCommand((_) =>
+            {
+                Data.Add(new LogEntry("empty", "empty", "empty"));
+
+            });
+
+            /*DeleteCommand = new RelayCommand((_) =>
+            {
+                int last = Data.Count;
+                Data.RemoveAt(last);
+                //OnPropertyChanged(nameof(CurrentTitle));
+
+            });*/
+
+            LoadLogs();
+        }
+
+        private void LoadLogs()
+        {
+            Data.Add(new LogEntry("10.02.2022", "01:20:34", "100km"));
+            Data.Add(new LogEntry("12.03.2022", "02:30:22", "230km"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
