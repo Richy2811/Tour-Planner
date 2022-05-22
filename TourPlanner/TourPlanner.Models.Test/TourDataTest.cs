c8df8e454aff755f4f713d46a362bd1819580917
DanielSchild25 <daniel.schild25@hotmail.com>
2022-05-22 19:51:34 +0200

    9 Unit Tests added

---
+++ TourPlanner/TourPlanner.Models.Test/TourDataTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+namespace TourPlanner.Models.Test
+{
+    public class  TourDataTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestLogEntry()
+        {
+            // Arrange
+            int expectedId = 1;
+            string expectedTourName = "testName";
+            string expectedDescription = "testDescription";
+            string expectedStart = "testStart";
+            string expectedDestination = "testDestination";
+            string expectedTransportType = "testTransportType";
+            decimal expectedTourDistance = 100;
+            string expectedTime = "testTime";
+            string expectedImageName = "testImageName";
+
+            // Act
+            TourData testTour = new(expectedId, expectedTourName, expectedDescription, expectedStart, expectedDestination, expectedTransportType, expectedTourDistance, expectedTime, expectedImageName);
+
+            int actualId = testTour.ID;
+            string actualTourName = testTour.TourName;
+            string actualDescription = testTour.TourDescription;
+            string actualStart = testTour.Start;
+            string actualDestination = testTour.Destination;
+            string actualTransportType = testTour.TransportType;
+            decimal actualTourDistance = testTour.TourDistance;
+            string actualTime = testTour.Time;
+            string actualImageName = testTour.ImageName;
+
+            // Assert
+            Assert.AreEqual(expectedId, actualId);
+            Assert.AreEqual(expectedTourName, actualTourName);
+            Assert.AreEqual(expectedDescription, actualDescription);
+            Assert.AreEqual(expectedStart, actualStart);
+            Assert.AreEqual(expectedDestination, actualDestination);
+            Assert.AreEqual(expectedTransportType, actualTransportType);
+            Assert.AreEqual(expectedTourDistance, actualTourDistance);
+            Assert.AreEqual(expectedTime, actualTime);
+            Assert.AreEqual(expectedImageName, actualImageName);
+        }
+    }
+}
+
---
+++ TourPlanner/TourPlanner.Test/TourLogViewModelTest.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TourPlanner.ViewModels;
+
+namespace TourPlanner.Test
+{
+    public class TourLogViewModelTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestLogData_ShouldContainInitialList()
+        {
+            // Arrange
+            TourLogViewModel tourLogViewModel = new();
+
+            // Act
+
+            int expected = 0;
+            int actual = tourLogViewModel.LogData.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "LogData should contain 0 Logs!");
+        }
+
+        [Test]
+        public void TestLogDataAdd_ShouldAddToList()
+        {
+            // Arrange
+            TourLogViewModel tourLogViewModel = new();
+
+            // Act
+            tourLogViewModel.LogData.Add(new LogEntry(1, "Test1", "Test1", "Test1", "Test1", "10"));
+            tourLogViewModel.LogData.Add(new LogEntry(1, "Test2", "Test2", "Test2", "Test2", "10"));
+
+            int expected = 2;
+            int actual = tourLogViewModel.LogData.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "LogData should contain two Logs!");
+        }
+
+        [Test]
+        public void TestAddLog_ShouldAddToList()
+        {
+            // Arrange
+            TourLogViewModel tourLogViewModel = new();
+            var lastDataCount = tourLogViewModel.LogData.Count;
+
+            string[] info;
+            string IdFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\..\logid.txt";
+            if (!File.Exists(IdFile))
+            {
+                throw new ArgumentException("Does not exist.", nameof(IdFile));
+            }
+
+            IEnumerable<string> line = File.ReadLines(IdFile);
+            Console.WriteLine(string.Join(Environment.NewLine, line));
+            info = line.ToArray();
+            int expectedID = Int32.Parse(info[0]);
+
+            int id = expectedID - 1;
+
+            using (StreamWriter writeNewID = new StreamWriter(IdFile))
+            {
+                writeNewID.WriteLine(id.ToString());
+            }
+
+            // Act
+            tourLogViewModel.AddLog.Execute(null);
+            var expectedDataCount = lastDataCount + 1;
+            var currentDataCount = tourLogViewModel.LogData.Count;
+
+            string currentComment = tourLogViewModel.LogData[expectedDataCount - 1].Comment;
+            string expectedComment = "empty";
+            int currentID = tourLogViewModel.LogData[expectedDataCount - 1].ID;
+            string currentDate = tourLogViewModel.LogData[expectedDataCount - 1].Date;
+            string expectedDate = "empty";
+
+            // Assert
+            Assert.AreEqual(expectedDataCount, currentDataCount, "A new Log should be added!");
+            Assert.AreEqual(expectedComment, currentComment, "The new Log Comment should be empty");
+            Assert.AreEqual(expectedID, currentID);
+            Assert.AreEqual(expectedDate, currentDate, "The new Log Date Type should be empty");
+
+        }
+    }
+}
